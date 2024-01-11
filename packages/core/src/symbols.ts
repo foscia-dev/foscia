48a38256e46241e3003cb60843ebe9abbca78408
paul <paul.thebaud29@gmail.com>
2024-01-11 22:24:14 +0100

    chore: add "foscia" prefix to symbols description

--- packages/core/src/symbols.ts
+++ packages/core/src/symbols.ts
@@ -1,8 +1,8 @@
-export const SYMBOL_MODEL_PROP_PENDING = Symbol('model pending property');
-export const SYMBOL_MODEL_PROP_ID: unique symbol = Symbol('model ID property');
-export const SYMBOL_MODEL_PROP_ATTRIBUTE: unique symbol = Symbol('model attribute property');
-export const SYMBOL_MODEL_PROP_RELATION: unique symbol = Symbol('model relation property');
-export const SYMBOL_MODEL_RELATION_HAS_ONE: unique symbol = Symbol('model relation has one');
-export const SYMBOL_MODEL_RELATION_HAS_MANY: unique symbol = Symbol('model relation has many');
-export const SYMBOL_MODEL_CLASS: unique symbol = Symbol('model class');
-export const SYMBOL_MODEL_INSTANCE: unique symbol = Symbol('model instance');
+export const SYMBOL_MODEL_PROP_PENDING = Symbol('foscia: model pending property');
+export const SYMBOL_MODEL_PROP_ID: unique symbol = Symbol('foscia: model ID property');
+export const SYMBOL_MODEL_PROP_ATTRIBUTE: unique symbol = Symbol('foscia: model attribute property');
+export const SYMBOL_MODEL_PROP_RELATION: unique symbol = Symbol('foscia: model relation property');
+export const SYMBOL_MODEL_RELATION_HAS_ONE: unique symbol = Symbol('foscia: model relation has one');
+export const SYMBOL_MODEL_RELATION_HAS_MANY: unique symbol = Symbol('foscia: model relation has many');
+export const SYMBOL_MODEL_CLASS: unique symbol = Symbol('foscia: model class');
+export const SYMBOL_MODEL_INSTANCE: unique symbol = Symbol('foscia: model instance');
--- packages/shared/src/descriptors/makeDescriptorHolder.ts
+++ packages/shared/src/descriptors/makeDescriptorHolder.ts
@@ -1,6 +1,6 @@
 import type { DescriptorHolder } from '@foscia/shared/descriptors/types';
 
-export const SYMBOL_DESCRIPTOR_HOLDER = Symbol('descriptor holder');
+export const SYMBOL_DESCRIPTOR_HOLDER = Symbol('foscia: descriptor holder');
 
 export default function makeDescriptorHolder<T>(descriptor: PropertyDescriptor) {
   return { $FOSCIA_TYPE: SYMBOL_DESCRIPTOR_HOLDER, descriptor } as DescriptorHolder<T>;
