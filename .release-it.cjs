01ac85ad28434a8c5aaf6eda7b9fc61ac9864195
paul <paul.thebaud29@gmail.com>
2024-01-24 22:47:44 +0100

    chore: update changelog version header template

---
+++ .release-it.cjs
@@ -0,0 +1,36 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const headerPartial = fs.readFileSync(
+  path.resolve(__dirname, 'scripts/changelog/header.hbs'),
+).toString();
+
+module.exports = {
+  hooks: {
+    'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.',
+  },
+  npm: {
+    publish: false,
+  },
+  git: {
+    requireBranch: 'main',
+    commitMessage: 'chore: release v${version}',
+    tagName: 'v${version}',
+    tagAnnotation: 'Release v${version}',
+    requireCleanWorkingDir: false,
+  },
+  github: {
+    release: true,
+    releaseName: 'v${version}',
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: 'angular',
+      infile: 'CHANGELOG.md',
+      header: '# Changelog',
+      writerOpts: {
+        headerPartial,
+      },
+    },
+  },
+};
