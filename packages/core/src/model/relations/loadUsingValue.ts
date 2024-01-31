ce7bb7e25bd667e7628834091f262f01ea303eb6
paul <paul.thebaud29@gmail.com>
2024-01-31 20:38:06 +0100

    fix(core): relation loading won't trigger readonly error anymore

--- packages/core/src/model/relations/loadUsingValue.ts
+++ packages/core/src/model/relations/loadUsingValue.ts
@@ -1,3 +1,4 @@
+import forceFill from '@foscia/core/model/forceFill';
 import markSynced from '@foscia/core/model/snapshots/markSynced';
 import { ModelInstance, ModelRelationKey } from '@foscia/core/model/types';
 
@@ -6,8 +7,7 @@ export default function loadUsingValue<I extends ModelInstance>(
   relation: ModelRelationKey<I>,
   value: I[ModelRelationKey<I>],
 ) {
-  // eslint-disable-next-line no-param-reassign
-  instance[relation] = value;
+  forceFill(instance, { [relation]: value } as any);
 
   markSynced(instance, relation);
 }
