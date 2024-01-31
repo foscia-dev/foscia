a7cf22e7ff7226a70fa9191a52ca9f472582fd5e
paul <paul.thebaud29@gmail.com>
2024-01-31 21:23:01 +0100

    fix(jsonapi): handle recursive records when not inside included

--- packages/jsonapi/src/blueprints/makeJsonApiDeserializer.ts
+++ packages/jsonapi/src/blueprints/makeJsonApiDeserializer.ts
@@ -7,7 +7,7 @@ import {
   JsonApiNewResource,
 } from '@foscia/jsonapi/types';
 import { makeDeserializerRecordFactory, makeDeserializerWith } from '@foscia/serialization';
-import { makeIdentifiersMap, mapArrayable } from '@foscia/shared';
+import { isNil, makeIdentifiersMap, mapArrayable, wrap } from '@foscia/shared';
 
 export default function makeJsonApiDeserializer<
   Record extends JsonApiNewResource = JsonApiNewResource,
@@ -19,7 +19,10 @@ export default function makeJsonApiDeserializer<
     deserializer: makeDeserializerWith({
       extractData: (data: Data) => {
         const included = makeIdentifiersMap<string, ModelIdType, Record>();
-        data.included?.map((record) => included.put(record.type, record.id, record as Record));
+
+        [...wrap(data.data), ...wrap(data.included)].forEach(
+          (record) => !isNil(record.id) && included.put(record.type, record.id, record as Record),
+        );
 
         return {
           records: data.data,
