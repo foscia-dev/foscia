63e730014ba8b4b8be1947a1ba09ab8675436477
paul <paul.thebaud29@gmail.com>
2024-02-20 23:20:58 +0100

    fix(http): `configureRequest` will now correctly merge options
    see #20

--- packages/http/src/actions/context/enhancers/configureRequest.ts
+++ packages/http/src/actions/context/enhancers/configureRequest.ts
@@ -2,11 +2,48 @@ import { Action, context } from '@foscia/core';
 import consumeRequestConfig from '@foscia/http/actions/context/consumers/consumeRequestConfig';
 import { HttpRequestConfig } from '@foscia/http/types';
 
-export default function configureRequest(request: HttpRequestConfig) {
-  return async <C extends {}>(action: Action<C>) => action.use(context({
-    httpRequestConfig: {
-      ...consumeRequestConfig(await action.useContext(), null),
-      ...request,
-    },
-  }));
+/**
+ * Configure an HTTP request used by the HttpAdapter (see {@link makeHttpAdapterWith}).
+ * Some configuration options will be merged when possible (object query params,
+ * headers, transformers, etc.).
+ * This enhancer can be used to configure a full request object or preconfigure
+ * some common options (e.g. headers and transformers).
+ * Passing a [Fetch request object](https://developer.mozilla.org/docs/Web/API/Request)
+ * as `request` option will ignore any other configuration and request
+ * object will be directly passed to the adapter. Transformers will still be
+ * applied, but other automatic transformation or data passing (params, body, etc.)
+ * won't be applied.
+ *
+ * @param nextConfig
+ *
+ * @category Enhancers
+ */
+export default function configureRequest(nextConfig: HttpRequestConfig) {
+  return async <C extends {}>(action: Action<C>) => {
+    const prevRequestConfig = consumeRequestConfig(await action.useContext(), null);
+
+    return action.use(context({
+      httpRequestConfig: {
+        ...prevRequestConfig,
+        ...nextConfig,
+        headers: { ...prevRequestConfig?.headers, ...nextConfig?.headers },
+        params: typeof nextConfig.params === 'string' ? nextConfig.params : {
+          ...(typeof prevRequestConfig?.params === 'string' ? {} : prevRequestConfig?.params),
+          ...nextConfig.params,
+        },
+        requestTransformers: [
+          ...(prevRequestConfig?.requestTransformers ?? []),
+          ...(nextConfig?.requestTransformers ?? []),
+        ],
+        responseTransformers: [
+          ...(prevRequestConfig?.responseTransformers ?? []),
+          ...(nextConfig?.responseTransformers ?? []),
+        ],
+        errorTransformers: [
+          ...(prevRequestConfig?.errorTransformers ?? []),
+          ...(nextConfig?.errorTransformers ?? []),
+        ],
+      },
+    }));
+  };
 }
