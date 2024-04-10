746924c35b0f9f6c9418fb1b46e49d5538de4acf
paul <paul.thebaud29@gmail.com>
2024-04-10 20:34:22 +0200

    feat(jsonapi): add relation endpoints support to adapter

--- packages/jsonapi/src/blueprints/makeJsonApiAdapter.ts
+++ packages/jsonapi/src/blueprints/makeJsonApiAdapter.ts
@@ -1,5 +1,7 @@
-import { deepParamsSerializer } from '@foscia/http';
+import { ActionName, consumeAction } from '@foscia/core';
+import { clearEndpoint, deepParamsSerializer } from '@foscia/http';
 import { makeRestAdapterWith, RestAdapterConfig } from '@foscia/rest';
+import { optionalJoin } from '@foscia/shared';
 
 export default function makeJsonApiAdapter<Data = any>(
   config: Partial<RestAdapterConfig<Data>> = {},
@@ -7,6 +9,18 @@ export default function makeJsonApiAdapter<Data = any>(
   return {
     adapter: makeRestAdapterWith({
       baseURL: '/api/v1',
+      buildURL: (endpoint, context) => clearEndpoint(optionalJoin([
+        endpoint.baseURL,
+        endpoint.modelPath,
+        endpoint.idPath,
+        (([
+          ActionName.ATTACH_RELATION,
+          ActionName.UPDATE_RELATION,
+          ActionName.DETACH_RELATION,
+        ] as any[]).indexOf(consumeAction(context, null)!) !== -1 ? 'relationships' : null),
+        endpoint.relationPath,
+        endpoint.additionalPath,
+      ], '/')),
       serializeParams: deepParamsSerializer,
       defaultHeaders: {
         Accept: 'application/vnd.api+json',
