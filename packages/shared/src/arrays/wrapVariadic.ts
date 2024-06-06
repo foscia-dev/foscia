6e5d6fb8f19badf26bb77798b50b9d61116f5303
paul <paul.thebaud29@gmail.com>
2024-06-06 21:33:16 +0200

    refactor: simplify wrap variadic

--- packages/shared/src/arrays/wrapVariadic.ts
+++ packages/shared/src/arrays/wrapVariadic.ts
@@ -1,12 +1,8 @@
 import { ArrayableVariadic } from '@foscia/shared/types';
 
 export default function wrapVariadic<T>(...values: ArrayableVariadic<T>): T[] {
-  if (values.length === 1) {
-    if (Array.isArray(values[0])) {
-      return wrapVariadic(...values[0]);
-    }
-
-    return [values[0]];
+  if (values.length === 1 && Array.isArray(values[0])) {
+    return values[0];
   }
 
   return values as T[];
