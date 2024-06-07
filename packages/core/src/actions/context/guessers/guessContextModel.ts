f753e8d097a1021f8f2d4175219b307f844a2f7e
paul <paul.thebaud29@gmail.com>
2024-06-07 18:34:23 +0200

    feat(core): guess model can guess multiple models for polymorphism

--- packages/core/src/actions/context/guessers/guessContextModel.ts
+++ packages/core/src/actions/context/guessers/guessContextModel.ts
@@ -1,13 +1,25 @@
-import guessRelationType from '@foscia/core/model/relations/guessRelationType';
+import guessRelationType from '@foscia/core/model/relations/utilities/guessRelationType';
 import { Model, ModelRelation } from '@foscia/core/model/types';
 import { RegistryI } from '@foscia/core/types';
 import { isNil, Optional, wrap } from '@foscia/shared';
 
+type GuessContextModelContext = {
+  model?: Optional<Model>;
+  relation?: Optional<ModelRelation>;
+  registry?: Optional<RegistryI>;
+  ensureType?: Optional<string>;
+};
+
 function guessModelIn(
   model: Optional<Model | Model[]>,
-  ensureType?: Optional<string>,
+  ensureType: Optional<string>,
+  multiple: boolean,
 ) {
   const models = wrap(model);
+  if (multiple) {
+    return models;
+  }
+
   if (isNil(ensureType)) {
     if (models.length > 1) {
       return null;
@@ -19,17 +31,23 @@ function guessModelIn(
   return models.find((m) => m.$type === ensureType) ?? null;
 }
 
-export default async function guessContextModel(
-  context: {
-    model?: Optional<Model>;
-    relation?: Optional<ModelRelation>;
-    registry?: Optional<RegistryI>;
-    ensureType?: Optional<string>;
-  },
-) {
+function guessContextModel(
+  context: GuessContextModelContext,
+  multiple?: false,
+): Promise<Model | null>;
+
+function guessContextModel(
+  context: GuessContextModelContext,
+  multiple: true,
+): Promise<Model[]>;
+
+async function guessContextModel(
+  context: GuessContextModelContext,
+  multiple: boolean = false,
+): Promise<Model[] | Model | null> {
   if (context.relation) {
     if (context.relation.model) {
-      return guessModelIn(await context.relation.model(), context.ensureType);
+      return guessModelIn(await context.relation.model(), context.ensureType, multiple);
     }
 
     const { registry } = context;
@@ -42,11 +60,17 @@ export default async function guessContextModel(
         possibleTypes.map((type) => registry.modelFor(type)),
       );
 
-      return guessModelIn(possibleModels.filter((m) => !!m) as Model[], context.ensureType);
+      return guessModelIn(
+        possibleModels.filter((m) => !!m) as Model[],
+        context.ensureType,
+        multiple,
+      );
     }
 
     return null;
   }
 
-  return guessModelIn(context.model, context.ensureType);
+  return guessModelIn(context.model, context.ensureType, multiple);
 }
+
+export default guessContextModel;
