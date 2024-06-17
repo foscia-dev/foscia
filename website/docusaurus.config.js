17eabf6443c41bead69bc8958bc1ab88d6a2c1ed
paul <paul.thebaud29@gmail.com>
2024-06-17 22:22:02 +0200

    docs: fix version in website nav

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -123,7 +123,9 @@ const config = {
           },
           {
             position: 'right',
-            label: `${process.env.VERSION} v${packageJson.version}` || `v${packageJson.version}`,
+            label: process.env.VERSION
+              ? `${process.env.VERSION} v${packageJson.version}`
+              : `v${packageJson.version}`,
             to: '/docs/upgrade/changelog',
             className: 'header-version-link button border--gradient',
           },
