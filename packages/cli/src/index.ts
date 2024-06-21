7ea530ea412a9007e488723f2ad06ea77ca08033
paul <paul.thebaud29@gmail.com>
2024-06-21 02:31:32 +0200

    feat(cli): support disabling colors

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,12 @@
 import kernel from '@foscia/cli/kernel';
+import c from 'ansi-colors';
 import process from 'node:process';
 
-(() => kernel(process.argv))();
+(async () => {
+  const colorsEnabled = !process.env.NO_COLOR && process.env.FORCE_COLOR !== '0';
+  if (!colorsEnabled) {
+    c.enabled = false;
+  }
+
+  await kernel(process.argv);
+})();
